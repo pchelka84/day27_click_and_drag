249d5e8ce2c5805cc61caf7d8aa0273a954dcf01
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-04 19:57:15 -0500

    Calculate how far we deviated from the initial point

--- scripts.js
+++ scripts.js
@@ -4,10 +4,11 @@ let startX;
 let scrollLeft;
 
 
-slider.addEventListener('mousedown', () => {
+slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
-  
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
 }); 
 
 slider.addEventListener('mouseleave', () => {
@@ -21,7 +22,13 @@ slider.addEventListener('mouseup', () => {
   slider.classList.remove('active');
 }); 
 
-slider.addEventListener('mousemove', () => {
+slider.addEventListener('mousemove', (e) => {
+// Stop function from running
  if (!isDown) return;
-
+ e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  // console.log({x, startX});
+  // Calculate how far we deviated from the initial point 
+  const walk = x - startX;
+  slider.scrollLeft = scrollLeft - walk;
 })
